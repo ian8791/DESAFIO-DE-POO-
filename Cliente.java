63530cb92bfb8c9522f26ba70c8ef24989a7e6eb
ian8791 <ianpinheiro8791@gmail.com>
2026-04-07 13:59:51 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,21 @@
+﻿public class Cliente {
+    private String nomeCompleto;
+    private int mesa;
+    public Cliente(String nomeCompleto, int mesa) {
+        this.nomeCompleto = nomeCompleto;
+        this.mesa = mesa;
+    }
+    public String getPrimeiroNome() {
+        int espacoIndex = nomeCompleto.indexOf(" ");
+        if (espacoIndex != -1) {
+            return nomeCompleto.substring(0, espacoIndex);
+        }
+        return nomeCompleto;
+    }
+    public String nomeMaiusculo() {
+        return nomeCompleto.toUpperCase();
+    }
+    public int getMesa() {
+        return mesa;
+    }
+}
---
+++ Pedido.java
@@ -0,0 +1,28 @@
+public class Pedido {
+    private String descricao;
+    private double valorUnitario;
+    private int quantidade;
+
+    public Pedido(String descricao, double valorUnitario, int quantidade) {
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotalItem() {
+        return valorUnitario * quantidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+}
+ 
